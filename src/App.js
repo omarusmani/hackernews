d5d47bc4272c5ccb4ae3efbb45dfe5e108148f8f
MarisolM09 <mmendez9980@gmail.com>
2022-08-17 19:57:21 -0500

    fixed css styling, merge issues, and added search input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import './App.css';
-import { formatDistance, } from 'date-fns'
+import "./App.css";
+import { formatDistance } from "date-fns";
 import React, { useEffect, useState } from "react";
-
 
 function App() {
   const [articles, setArticles] = useState([]);
 
-
-  async function getArticles(tag,query,bydate,page,hits) {
+  async function getArticles(tag, query, bydate, page, hits) {
     // console.log(`http://hn.algolia.com/api/v1/search${bydate}?query=${query}&tags=${tag}&hitsPerPage=${hits}&page=${page}`)
-    const response = await fetch(`http://hn.algolia.com/api/v1/search${bydate}?query=${query}&tags=${tag}&hitsPerPage=${hits}&page=${page}`);//need to add date range
+    const response = await fetch(
+      `http://hn.algolia.com/api/v1/search${bydate}?query=${query}&tags=${tag}&hitsPerPage=${hits}&page=${page}`
+    ); //need to add date range
 
     const data = await response.json();
-    setArticles(data.hits);//move to new file for calling including uses effect
+    setArticles(data.hits); //move to new file for calling including uses effect
   }
 
   useEffect(() => {
     console.log("Mounted");
-    getArticles("","","","","");//fixed error where no queries where causing issues with api rejection//this also brings the initial page 
+    getArticles("", "", "", "", ""); //fixed error where no queries where causing issues with api rejection//this also brings the initial page
   }, []);
 
   useEffect(() => {
@@ -32,102 +31,120 @@ function App() {
     //  return date.toLocaleDateString("en-US")
     return formattedDate;
   }
-  function getUrl(props){
+  function getUrl(props) {
     // console.log(props.url==null)
     //  console.log( props._url === null ? `https://news.ycombinator.com/item?id=${props.objectID}` : `${props.url}`)
-     return (props.url === null ? `https://news.ycombinator.com/item?id=${props.objectID}` :props.url)
-
+    return props.url === null
+      ? `https://news.ycombinator.com/item?id=${props.objectID}`
+      : props.url;
   }
 
   return (
     <body>
       <section>
-        <div>
-          <article>
+        <div className="App">
+          <div>
             <h1>
               <a href="https://news.ycombinator.com/" className="favicon">
                 H
               </a>
               Search<br></br>
               Hacker News
+              <input
+                className="search"
+                type="text"
+                placeholder="Search stories by title, url, or author"
+                name="search"
+              ></input>
             </h1>
-          </article>
 
-          <form>
-            <div>Search</div>
-            <button></button>
-            <div>by</div>
-            <button></button>
-            <div>for</div>
-            <button></button>
-          </form>
-          <article>
-            <div>
-              <ul>
-                {articles.map((article, index) => (
-                  <li className="posts" key={article.objectID}>
-                    <h2>
-                      <a href={article.url}>{article.title}</a>
-                      <span>({article.url})</span>
-                    </h2>
+            <button onClick={() => getArticles("", "", "", "", "")}>
+              Hello
+            </button>
+            <button onClick={() => getArticles("comment", "", "", "", "")}>
+              Hello
+            </button>
+            <button onClick={() => getArticles("ask_hn", "", "", "", "")}>
+              Hello
+            </button>
+            <button onClick={() => getArticles("front_page", "", "", "", "")}>
+              Hello
+            </button>
 
-    <div className="App">
-      <div>
-      <h1>
-        <a href="https://news.ycombinator.com/"className="favicon">H</a>
-        Search<br></br>
-        Hacker News</h1>
-        <button onClick={()=>getArticles("","","","","")}>Hello</button>
-        <button onClick={()=>getArticles("comment","","","","")}>Hello</button>
-        <button onClick={()=>getArticles("ask_hn","","","","")}>Hello</button>
-        <button onClick={()=>getArticles("front_page","","","","")}>Hello</button>
+            <form>
+              <div>Search</div>
+              <select>
+                <option value="blue">Stories</option>
+                <option value="green">All</option>
+                <option value="red">Comments</option>
+              </select>
+              <div>by</div>
+              <select>
+                <option value="blue">Popularity</option>
+                <option value="green">Date</option>
+              </select>
+              <div>for</div>
+              <select>
+                <option
+                  onClick={() => getArticles("", "yellow", "", "", "")}
+                  value="blue"
+                >
+                  All time
+                </option>
+                <option value="green">Last 24h</option>
+                <option value="red">Past Week</option>
+                <option value="yellow">Past Month</option>
+                <option value="yellow">Past Year</option>
+                <option value="yellow">Custom range</option>
+              </select>
+            </form>
 
-        <form>
-        <div>Search</div>
-    <select>
-      <option value='blue'>Stories</option>
-      <option value='green'>All</option>
-      <option value='red'>Comments</option>
-    </select>
-        <div>by</div>
-        <select>
-      <option value='blue'>Popularity</option>
-      <option value='green'>Date</option>
-    </select>
-        <div>for</div>
-      <select>
-      <option onClick={()=>getArticles("","yellow","","","")} value='blue'>All time</option>
-      <option value='green'>Last 24h</option>
-      <option value='red'>Past Week</option>
-      <option value='yellow'>Past Month</option>
-      <option value='yellow'>Past Year</option>
-      <option value='yellow'>Custom range</option>
+            <ul>
+              {articles.map((article, index) => (
+                <li className="posts" key={article.objectID}>
+                  <h2>
+                    <a
+                      href={`https://news.ycombinator.com/item?id=${article.objectID}`}
+                    >
+                      {article.title}
+                    </a>
+                    <span>
+                      <a href={article.url} className="url">
+                        {getUrl(article)}
+                      </a>{" "}
+                    </span>
+                  </h2>
 
-    </select>
-        </form>
-      <ul>
-       {articles.map((article, index)=>(
-        <li className="posts" key={article.objectID}>
-            <h2><a href={`https://news.ycombinator.com/item?id=${article.objectID}`}>{article.title}</a></h2>
-            {/* {/* <span>{index}</span> */}
-            <a href={article.url} className="url">{getUrl(article)}</a> {/*add href fot article link on hackernews */}
-            <span className='bottom'>
-            <br></br>
-            <a href={`https://news.ycombinator.com/item?id=${article.objectID}`}>{article.points}</a> {/*add two classes in new folder */}
-            <div>|</div>
-            <div>{article.author}</div>
-            <div>|</div>
-            <a href={`https://news.ycombinator.com/item?id=${article.objectID}`}>{getDate(article.created_at_i)}</a>
-            <div>|</div>
-            <a href={`https://news.ycombinator.com/item?id=${article.objectID}`}>{`${article.num_comments} comments`}</a>
-            </span>
-            <br></br>
-        </li>
-        ))}
-      </ul>
-      </div>
-    </div>
-
+                  {/*add href fot article link on hackernews */}
+                  
+                    <p>
+                    <a
+                      href={`https://news.ycombinator.com/item?id=${article.objectID}`}
+                    >
+                      {article.points}
+                    </a>{" "}
+                    {/*add two classes in new folder */}
+                    <div>|</div>
+                    {article.author}
+                    <div>|</div>
+                    <a
+                      href={`https://news.ycombinator.com/item?id=${article.objectID}`}
+                    >
+                      {getDate(article.created_at_i)}
+                    </a>
+                    <div>|</div>
+                    <a
+                      href={`https://news.ycombinator.com/item?id=${article.objectID}`}
+                    >{`${article.num_comments} comments`}</a>
+                  </p>
+                  
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </body>
   );
 }
 
